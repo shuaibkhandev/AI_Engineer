dae45541ecdf76b9a210f18dc8de32387eba2359
shuaibkhan <skhan.csit@gmail.com>
2026-06-26 10:27:59 +0500

    invoked LLM

---
+++ invoke_llm/app.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+import OpenAI from "openai";
+
+const client = new OpenAI({
+    apiKey: process.env.GROQ_API_KEY,
+    baseURL: "https://api.groq.com/openai/v1",
+});
+
+const response = await client.responses.create({
+    model: "openai/gpt-oss-20b",
+    input: "Hey",
+});
+
+console.log(response.output_text);

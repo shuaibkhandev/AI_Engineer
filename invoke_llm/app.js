8445dce3ffa65734914e23a4328f9e1f24247a3c
shuaibkhan <skhan.csit@gmail.com>
2026-06-28 22:20:54 +0500

    worked with structer data

--- invoke_llm/app.js
+++ invoke_llm/app.js
@@ -9,15 +9,22 @@ async function main() {
     temperature:1,
     // top_p:0.2, alternative of temperature
     max_completion_tokens:100,
+    // stop:"",
+    // frequency_penalty:1,
+    response_format:{"type":"json_object"},
     model: "openai/gpt-oss-20b",
     messages: [
         {
             role: "system",
-            content: "### You are Shuaib_AI, a JavaScript language teacher ###"
+            content: `You are Shuaib_AI, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review is Positive, Negative or Neutral. ### You must return the result in valid JSON structure. ###
+            example: {"sentiment": "Negative"}
+            `,
         },
       {
         role: "user",
-        content: "Who are you?",
+        content: `Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week.
+        Sentiment
+        `,
       },
     ],
   });

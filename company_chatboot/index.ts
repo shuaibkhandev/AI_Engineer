dd533842538e07a1517a7cc67d8458bfe74cb71c
shuaibkhan <skhan.csit@gmail.com>
2026-07-03 14:57:47 +0500

    write implementation plan

--- company_chatboot/index.ts
+++ company_chatboot/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+/**
+ * 
+ * Implementation Plan
+ * 
+ * Stage 1: Indexing
+ * 1. Load the document - pdf, text
+ * 2. Chunk the document
+ * 3. Generate Vector Embeddings
+ * 4. Store the Vector Embeddings in Vector Database
+ * 
+ * Stage 2: Using the Chatboot
+ * 1. Setup LLM
+ * 2. Add Retrieval Step
+ * 3. Pass input + relevant information to LLM
+ * DONE
+ * 
+ */

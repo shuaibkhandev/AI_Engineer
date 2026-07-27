a215ab379dfab8a41ca228c626f4a33c27dc16bb
shuaibkhandev <skhan.csit@gmail.com>
2026-07-27 15:28:27 +0500

    add custom tool

--- Agent_with_langGarph/agent.js
+++ Agent_with_langGarph/agent.js
@@ -3,6 +3,8 @@ dotenv.config();
 import { ChatGroq } from "@langchain/groq";
 import { createAgent } from "langchain";
 import { TavilySearch } from "@langchain/tavily";
+import * as z from "zod"
+import { tool } from "langchain"
 
 
 async function main() {
@@ -16,15 +18,30 @@ async function main() {
   topic: "general",
 });
 
+const calendarEvents = tool(
+  ({ query, limit }) => {
+    return JSON.stringify([{title:"Meeting with John", time:"2 PM", location:"London"},{title:"Meeting with Akshy", time:"5 PM", location:"India"}])
+  },
+  {
+    name: "get_calendar_events",
+    description: "Call to get the calendar events",
+    schema: z.object({
+      query: z.string().describe("The query to use in calendar event search."),
+      limit: z.number().describe("Maximum number of results to return"),
+    }),
+  }
+);
+
+
   const agent = createAgent({
     model,
-    tools: [search],
+    tools: [search, calendarEvents],
     systemPrompt: "You are a helpful assistant.",
   }); 
 
   const aiMsg = await agent.invoke({
      messages : [
-  { role: "user",  content: "What is the current weather in kpk swat barikot Paksitan?" },
+  { role: "user",  content: "Current Weather of Peshawar" },
 ]})
 
 console.log("Assistant:",aiMsg.messages[aiMsg.messages.length-1].content);
